c9608aacb408d5a2e870285d89e969025a68f1e2
锐神 <370984703@qq.com>
2024-12-13 20:27:41 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -30,7 +30,7 @@ let addressesnotls = [];
 let addressesnotlsapi = [];
 let addressescsv = [];
 let DLS = 8;
-let remarkIndex = 1;//CSV备注所在列偏移量
+let remarkIndex = 1;//CSV备注所在列偏移量。
 let FileName = atob('ZWRnZXR1bm5lbA==');
 let BotToken;
 let ChatID; 
